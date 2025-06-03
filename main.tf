5f5bf6c4e8975a6e861e08e8a0f2c06077086093
JunyaTaniai <surf.0530.junya@gmail.com>
2025-06-03 13:26:56 +0900

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-027fff96cc515f7bc"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terraform-example"
+  }
+}
