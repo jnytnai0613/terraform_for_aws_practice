4870b4757bc0c4fa7c462bcbdc454d7c6aa73029
JunyaTaniai <surf.0530.junya@gmail.com>
2025-06-03 20:15:36 +0900

    Modify ec2 to asg

--- varibles.tf
+++ varibles.tf
@@ -1,5 +1,17 @@
-variable "server_port" {
+variable "asg_server_port" {
     description = "The Port the server will use for HTTP requests"
     type = number
     default = 8080
+}
+
+variable "alb_ingress_port" {
+    description = "The Port the load balancer will use for HTTP requests"
+    type = number
+    default = 80
+}
+
+variable "alb_egress_port" {
+    description = "The Port the load balancer will use for HTTP response"
+    type = number
+    default = 0
 }
