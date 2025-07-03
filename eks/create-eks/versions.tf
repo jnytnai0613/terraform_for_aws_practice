01404c9a60b4e142bd785401526a0c3b19b1015a
jnytnai0613 <surf.0530.junya@gmail.com>
2025-07-03 17:15:17 +0900

    style: 💄 provider変更

--- eks/create-eks/versions.tf
+++ eks/create-eks/versions.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">=1.12.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.95.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-northeast-1"
 }
